9064a53b5624688bb8e53e3b8750a8fa83915c08
Andrew <andrew-jung@hotmail.com>
2025-11-10 21:40:18 -0800

    Add page for user badges

--- components/Screens/UserBadgesPage.jsx
+++ components/Screens/UserBadgesPage.jsx
@@ -1,11 +1,99 @@
-import { View, Text, SafeAreaView, ScrollView} from 'react-native'
+import { View, Text, SafeAreaView, ScrollView, FlatList, TouchableOpacity} from 'react-native'
 import React from 'react'
 
 const UserBadgesPage = () => {
+
+  const fakeBadges = [
+    {
+      name : 'The Critic',
+      unlockedLevel : "NONE",
+      progressCompleted : 3,
+      progressNeeded  :10,
+      currentLevel: 'BRONZE'
+    },
+    {
+      name : 'The Tastemaker',
+      unlockedLevel : "NONE",
+      progressCompleted : 1,
+      progressNeeded  :20,
+      currentLevel: 'NONE'
+
+    }, {
+      name : 'The Historian',
+      unlockedLevel : "NONE",
+      progressCompleted : 19,
+      progressNeeded  :25,
+      currentLevel: 'BRONZE'
+
+    }, 
+    {
+      name : "The People's Choice",
+      unlockedLevel : "NONE",
+      progressCompleted : 13,
+      progressNeeded  :25,
+      currentLevel: 'GOLD'
+
+    },
+    {
+      name : 'The Conversationalist',
+      unlockedLevel : "NONE",
+      progressCompleted : 15,
+      progressNeeded  :18,
+      currentLevel: 'BRONZE'
+
+    },
+    {
+      name : 'The Auteur',
+      unlockedLevel : "NONE",
+      progressCompleted : 12,
+      progressNeeded  :21,
+      currentLevel: 'NONE'
+    },
+    {
+      name : "The People's Choice",
+      unlockedLevel : "NONE",
+      progressCompleted : 13,
+      progressNeeded  :25,
+      currentLevel: 'NONE'
+      
+    },
+    
+
+  ]
   return (
     <SafeAreaView className='bg-primary w-full h-full'>
-        <ScrollView>
-            <Text className='text-white'>UserBadgesPage</Text>
+        <ScrollView showsVerticalScrollIndicator={false} >
+            <View className='items-center justify-center w-full'>
+              <Text className='text-white'>UserBadgesPage</Text>
+              <FlatList
+                data={fakeBadges}
+                scrollEnabled={false}
+                showsVerticalScrollIndicator={false}
+                keyExtractor={(item,index) => index}
+                contentContainerStyle ={{flexWrap:'wrap', display:'flex', flexDirection:'row',width:'100%', gap:15, justifyContent:'start', alignItems:'center', paddingBottom:100, paddingTop:100}}
+                renderItem={({item}) => {
+                  const percentage = Math.floor((item.progressCompleted/item.progressNeeded)*100)
+                  return (
+                  <TouchableOpacity className={`bg-primaryLight w-[160px] h-[230px] px-4 rounded-2xl justify-center items-center gap-3 relative ${item.currentLevel === 'NONE' ? 'opacity-30' : ''} `}>
+                    { item.currentLevel !== 'NONE' && (
+                      <Text className='bg-secondary rounded-full px-2 py-1 text-primary text-xs absolute left-3 top-3'>{item.currentLevel}</Text>
+                    )}
+                    <View className={`w-[60px] h-[60px] rounded-full bg-primary ${item.currentLevel === 'NONE' ? 'opacity-40' : ''}`} />
+                    <View className='gap-3'>
+                      <Text className='text-mainGray font-bold'>{item.name}</Text>
+                      <Text className='text-mainGray text-sm '>{item.progressCompleted}/{item.progressNeeded} until next level</Text>
+                      <View style={{}} className='w-[110px] rounded-full bg-primary h-[10px] relative'>
+                        <View style={{width:`${percentage}%`}} className='bg-newLightGray rounded-full h-[10px] absolute ' />
+                      </View>
+                    </View>
+              
+                  </TouchableOpacity>
+                )}
+                }
+                  
+              
+              />
+            </View>
         </ScrollView>
     </SafeAreaView>
   )
